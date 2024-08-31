4553eb00bc0595420a7c7e4457076e56375a3e08
Shubham Kedia <shubham.kedia@vegrow.in>
2024-08-31 17:42:38 +0530

    configuration updated

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,26 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left toggle<CR>', {})
-      vim.keymap.set('n', '<C-s>', ':Neotree<CR>', {})
-    end
-  }
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		local neo_tree = require("neo-tree")
+		neo_tree.setup({
+			filesystem = {
+				filtered_items = {
+					visible = false,
+					hide_dotfiles = false,
+					hide_by_name = {
+						".git",
+					},
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left toggle<CR>", {})
+		vim.keymap.set("n", "<C-s>", ":Neotree<CR>", {})
+	end,
+}
