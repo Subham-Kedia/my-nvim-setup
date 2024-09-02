7cfaf5bbe9e3dfa27bc4b16e2ddb805cff84a3ba
Shubham Kedia <shubham.kedia@vegrow.in>
2024-09-02 20:03:49 +0530

    added lazy git

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -4,23 +4,37 @@ return {
     -- Define a global variable to hold the token
     _G.auth_token = nil
 
-    -- Setup rest.nvim
-    require("rest-nvim").setup({
-      result = {
-        -- Function to handle the response and extract the token
-        format = function(response)
-          -- Extract the token from the 'Authorization' or a custom header
-          _G.auth_token = response.headers["Authorization"] or response.headers["x-auth-token"]
-
-          -- If the token has a 'Bearer' prefix, remove it
-          if _G.auth_token and _G.auth_token:match("^Bearer ") then
-            _G.auth_token = _G.auth_token:sub(8)
-          end
-
-          -- Optionally print the token for debugging
-          print("Captured Token:", _G.auth_token)
-        end,
-      },
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "json",
+      callback = function(ev)
+        vim.bo[ev.buf].formatprg = "jq"
+        print("It's a json file")
+      end,
     })
+
+    vim.g.rest_nvim = {
+      response = {
+
+        hooks = {
+          -- Function to handle the response and extract the token
+          format = function(response)
+            -- Extract the token from the 'Authorization' or a custom header
+            _G.auth_token = response.headers["Authorization"] or response.headers["x-auth-token"]
+
+            -- If the token has a 'Bearer' prefix, remove it
+            if _G.auth_token and _G.auth_token:match("^Bearer ") then
+              _G.auth_token = _G.auth_token:sub(8)
+            end
+
+            -- Optionally print the token for debugging
+            print("Captured Token:", _G.auth_token)
+          end,
+        },
+      },
+    }
+    vim.keymap.set("n", "<leader>ro", ":Rest open<CR>", { desc = "Rest open" })
+    vim.keymap.set("n", "<leader>rr", ":Rest run<CR>", { desc = "Rest run" })
+    vim.keymap.set("n", "<leader>rl", ":Rest logs<CR>", { desc = "Rest logs" })
+    vim.keymap.set("n", "<leader>res", ":Rest env select<CR>", { desc = "Rest env select" })
   end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,8 +5,17 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 
+-- Enable Tree-sitter-based folding
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldlevelstart = 99
+
 -- vertical split
 vim.keymap.set("n", "<Leader>v", ":vsp<CR>", { desc = "v split", noremap = true, silent = true })
 
 -- handling window movement
 vim.keymap.set("n", "<Leader>w", "<C-w>", { desc = "window", noremap = true, silent = true })
+-- handling tabs
+vim.keymap.set("n", "<Leader>tn", ":tabnew<CR>", { desc = "new tab", noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>tc", ":tabclose<CR>", { desc = "close tab", noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>ts", ":tab split<CR>", { desc = "split tab", noremap = true, silent = true })
