39e4bfe0da7c1acbb35b70843a3a3c1722465b03
Shubham Kedia <shubham.kedia@vegrow.in>
2024-09-04 17:50:16 +0530

    gitsigns updated

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,16 +1,19 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require('gitsigns').setup({
-      signs = {
-    add          = { text = '│' },
-    change       = { text = '│' },
-    delete       = { text = '_' },
-    topdelete    = { text = '‾' },
-    changedelete = { text = '~' },
-    untracked    = { text = '┆' },
-  },
-    })
-    vim.keymap.set('n', '<leader>hp', 'preview_hunk', {})
-  end,
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		local gitsigns = require("gitsigns")
+		-- check default configuration
+		-- :help gitsigns-usage
+
+		local function nav_hunk()
+			gitsigns.nav_hunk("next", { wrap = true, preview = true })
+		end
+
+		gitsigns.setup({
+			current_line_blame = true,
+		})
+		vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { desc = "hunk preview" })
+		vim.keymap.set("n", "<leader>hd", gitsigns.diffthis, { desc = "buffer diff" })
+		vim.keymap.set("n", "<leader>hn", nav_hunk, { desc = "buffer diff" })
+	end,
 }
