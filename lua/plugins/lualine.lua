deb3491ac1235c2bacb84b6d44f23e751e12b7d2
Shubham Kedia <shubham.kedia@vegrow.in>
2024-09-04 17:54:32 +0530

    configuration updated surround plugin added

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,13 @@
+-- fast and easy to configure statusline plugin for neovim
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'dracula'
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- for supporting icons in status line
+	config = function()
+		local lualine = require("lualine")
+		lualine.setup({
+			options = {
+				theme = "dracula",
+			},
+		})
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,41 +1,47 @@
 -- telescope finding files, searching texts with ripgrep, help section, buffers
-
+-- fuzzy finding | search, filter, and pick things in lua
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
-		config = function()
-			local telescope = require("telescope")
-			telescope.setup({
-				defaults = {
-					file_ignore_patterns = {
-						".git",
-					},
-				},
-			})
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", function()
-				builtin.find_files({ hidden = true })
-			end, { desc = "Find Files" })
-			vim.keymap.set("n", "<leader>fg", function()
-				builtin.live_grep({ previewer = true })
-			end, { desc = "live grep" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "buffers" })
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "help tags" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup({
+        defaults = {
+          sorting_strategy = "descending",  --descending | ascending
+          file_ignore_patterns = {
+            ".git/",
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", function()
+        builtin.find_files({ hidden = true })
+      end, { desc = "Find Files" })
+      vim.keymap.set("n", "<leader>fg", function()
+        builtin.live_grep({ previewer = true })
+      end, { desc = "live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "help tags" })
+      vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "resume telescope" })
+    end,
+    -- builtin functions
+    -- git_branches
+    -- git_status
+    -- resume
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
