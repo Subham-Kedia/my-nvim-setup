1b532bb5de8bfaca18d622a7c8883c177729fec8
Shubham Kedia <shubham.kedia@vegrow.in>
2024-09-05 16:55:19 +0530

    hidden files config updated

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,13 @@ return {
 	},
 	config = function()
 		local oil = require("oil")
-		oil.setup()
+		oil.setup({
+			view_options = {
+				is_hidden_file = function(name)
+					return vim.startswith(name, ".git")
+				end,
+			},
+		})
 		vim.keymap.set("n", "-", oil.toggle_float, { desc = "oil" })
 	end,
 }
